186be989ba300c26581d185a4723f1f77ff75fdd
Ben Scherrey <scherrey@proteus-tech.com>
2018-12-31 18:37:25 +0700

    Cleaned up types in parser.py.

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,8 @@
 #
 #   parser.py   -   Parser for our language.
 #
-from typing import List
+#from __future__ import annotations
+from typing import List, TextIO, Optional
 
 class Parser:
 
@@ -10,18 +11,18 @@ class Parser:
         if filename:
             self.open(filename)
 
-    def reset(self):
+    def reset(self) -> "Parser":
         self.linenum = 0
         self.column = 0
         self.filename = str("")
-        self.file_handle = None
-        self.content = []
+        self.file_handle : Optional[TextIO] = None
+        self.content : List[str] = []
         return self
 
     def open(self, filename: str):
         self.reset()
         self.filename = filename
-        self.file_handle = open(filename)
+        self.file_handle : Optional[TextIO] = open(filename)
         self.content = self.file_handle.readlines()
         return self
 
