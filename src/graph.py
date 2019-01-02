e9c7b56b465dd04c867d29d8e5d780e56b3952c6
Ben Scherrey <scherrey@proteus-tech.com>
2019-01-02 15:08:16 +0700

    Smarter Location default filename handling.

--- src/graph.py
+++ src/graph.py
@@ -5,10 +5,12 @@ from enum import Enum
 
 class Location:
 
-    def __init__(self, filename = "Unknown", linenum: int = 0, column: int = 0) -> None:
+    def __init__(self, filename = None, linenum: int = 0, column: int = 0) -> None:
         self.filename = filename
         self.linenum = linenum
         self.column = column
+        if not filename:
+            self.filename = "Unknown"
 
 class Symbol:
 
