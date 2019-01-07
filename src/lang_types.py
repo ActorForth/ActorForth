a3ea55d80be91b2d9e9a68f414fa84aba97f494e
Ben Scherrey <scherrey@proteus-tech.com>
2019-01-07 21:36:56 +0700

    Got Literal and Nat working and tested.

--- src/lang_types.py
+++ src/lang_types.py
@@ -1,10 +1,35 @@
 #
 #   types.py    -   Built in types for our language.
 #
+from typing import Iterable, Any, Union
+
+def is_constexpr( exprs: Iterable ) -> bool:
+    return all([i.is_constexpr() for i in exprs])
 
 class Nat:
 
-    #@static 
-    def isa( value ) -> bool:
+    def __init__(self, value : int) -> None:
+        if Nat.isa(value):
+            self.value = value
+        else:            
+            raise ValueError("'%s' is not a valid instance of the Nat type. Nats are positive integers only 0..Inf." % value)
+
+    def isa( value : Any ) -> bool:
         return isinstance(value, int) and value >= 0
 
+    def is_constexpr() -> bool:
+        return False
+
+class Literal:
+
+    def __init__(self, value : Union[int, str]) -> None:
+        if Literal.isa(value):
+            self.value = value
+        else: 
+            raise ValueError("'%s' is not a valid instance of the Literal type. Literals are const strings or numbers only." % value)
+
+    def isa( value : Any ) -> bool:
+        return isinstance(value,  int) or isinstance(value, str)
+
+    def is_constexpr() -> bool:
+        return True        
--- src/tests/test_lang_types.py
+++ src/tests/test_lang_types.py
@@ -1,6 +1,6 @@
-import unittest
+import unittest, pytest
 
-from lang_types import Nat 
+from lang_types import is_constexpr, Nat, Literal 
 
 class TestTypes( unittest.TestCase) :
 
@@ -10,3 +10,37 @@ class TestTypes( unittest.TestCase) :
         assert Nat.isa(-1) is False
         assert Nat.isa('a') is False
         assert Nat.isa(1.0) is False
+
+    def test_Nat_ctor(self) -> None:
+        n = Nat(0)
+        assert n.value is 0
+        with pytest.raises(ValueError) as x:
+            n = Nat(-1)
+        with pytest.raises(ValueError) as x:
+            n = Nat("1")            
+
+
+    def test_Literal_isa(self) -> None:
+        assert Literal.isa(0) is True
+        assert Literal.isa('a') is True
+        assert Literal.isa(Nat) is False
+        assert Literal.isa(Literal) is False
+
+    def test_Literal_ctor(self) -> None:
+        n = Literal(0)
+        assert n.value is 0
+        s = Literal("I'm a string!")        
+        assert s.value is "I'm a string!"
+        with pytest.raises(ValueError) as x:
+            l = Literal(Nat)
+
+class TestTypeFunctions( unittest.TestCase ) :
+
+    def test_is_constexpr(self) -> None:
+        constexpr = [Literal, Literal]
+        non_constexpr = [Nat, Nat]
+        mixed_expr = constexpr + non_constexpr
+
+        assert is_constexpr(constexpr) is True
+        assert is_constexpr(non_constexpr) is False
+        assert is_constexpr(mixed_expr) is False
