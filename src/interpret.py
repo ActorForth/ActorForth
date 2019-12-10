8844f6e5c79d6726b166a4faa39be6f4df779e07
Ben Scherrey <scherrey@proteus-tech.com>
2019-12-10 21:39:46 +0700

    Initial basic stack and op input signature matching.

--- src/interpret.py
+++ src/interpret.py
@@ -11,12 +11,12 @@ def op_int(s: Stack, s_id: str) -> None:
     print("op_int(s_id = '%s')\n" % s_id )
     i = int(s.pop().value)
     s.push(StackObject(i,TInt))
-op_int.sig=TypeSignature([TAtom],[TAtom])
+op_int.sig=TypeSignature([TAtom],[TInt])
 
 def op_atom(s: Stack, s_id: str) -> None:
     print("op_atom(s_id = '%s')\n" % s_id) 
     s.push(StackObject(s_id,TAtom))
-op_atom.sig=TypeSignature([TAtom],[TAtom])
+op_atom.sig=TypeSignature([],[TAtom])
 
 def op_plus(s: Stack, s_id: str) -> None:
     print("op_plus(s_id = '%s')\n" % s_id) 
@@ -58,25 +58,37 @@ stack = Stack()
 p = Parser("samples/fundamentals01.a4")
 #p = Parser("samples/fib.a4")
 
-def check_input_type_sig(stack: Stack, op:Callable[[Stack, str],None]) -> bool:
-        return True
+def check_input_type_sig(stack: Stack, op: Callable[[Stack, str],None]) -> bool:
+    in_types = op.sig.stack_in
+    if not len(in_types) : return True
+    #stack_types = [s.type for s in reversed(stack.contents()[:len(in_types)]) ]
+    stack_types = [s.type for s in stack.contents()[len(in_types)*-1:] ]
+    print("in_types = %s" % in_types)
+    print("stack_types = %s" % stack_types)
+    return in_types == stack_types
+        
 
-def check_output_type_sig(stack: Stack, op:Callable[[Stack, str],None]) -> bool:
+def check_output_type_sig(stack: Stack, op: Callable[[Stack, str],None]) -> bool:
         return True        
 
 for token in p.tokens():
     symbol = Symbol(token[0], Location(p.filename,token[1],token[2]), TAtom)
     print(symbol)
     tos = stack.tos()
+    print("\nStack = %s" % stack.contents())
     if tos is not Stack.Empty:
         op, found = find_type_atom(tos.type,symbol.s_id)
         if not found:
             op, found = find_atom(symbol.s_id)
     else:
         op, found = find_atom(symbol.s_id)
-    print("Stack = %s : " % stack.contents())
+    
     try:
-        op(stack, symbol.s_id)
+        if check_input_type_sig(stack, op):
+            op(stack, symbol.s_id)
+            print("Stack = %s\n" % stack.contents())
+        else:
+            raise Exception("Stack content doesn't match Op TypeSignature.")
     except Exception as x:
         print("Exception %s" % x)
         print("Interpreting symbol %s" % symbol)
