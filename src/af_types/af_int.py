d99ff2dd786f69dd8eec8a809d0c2bcad8e0b8f3
Ben Scherrey <scherrey@proteus-tech.com>
2020-02-14 18:05:46 +0700

    Added int mulitiplication.

--- src/af_types/af_int.py
+++ src/af_types/af_int.py
@@ -38,7 +38,19 @@ def op_minus(s: Stack, s_id: str) -> None:
     op_int(s,s_id) # We're cheating here cause, for now, op_int is supposed to take a TAtom!
 op_minus.sig=TypeSignature([TInt,TInt],[TInt])
 
+def op_multiply(s: Stack, s_id: str) -> None:
+    print("op_minus(s_id = '%s')\n" % s_id) 
+    op1 = s.pop().value
+    op2 = s.pop().value
+    result = op2*op1
+    # Guarantee output is valid and not overflow.
+    assert int(result) / op1 == op2, "python math error"
+    s.push(StackObject(result,TInt))
+    op_int(s,s_id) # We're cheating here cause, for now, op_int is supposed to take a TAtom!
+op_multiply.sig=TypeSignature([TInt,TInt],[TInt])
+
 #   Int dictionary
 forth_dict.insert(0,('int',op_int))
 TInt.forth_dict.insert(0,('+',op_plus))
 TInt.forth_dict.insert(0,('-',op_minus))
+TInt.forth_dict.insert(0,('*',op_multiply))
