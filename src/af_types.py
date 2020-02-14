52c3f51e9acfb0524e9d496a21bd38bb5cd4fe7d
Ben Scherrey <scherrey@proteus-tech.com>
2020-02-14 15:06:25 +0700

    Some initial math type checking and addition of - operator.

--- src/af_types.py
+++ src/af_types.py
@@ -68,6 +68,8 @@ class TypeSignature:
 def op_int(s: Stack, s_id: str) -> None:
     print("op_int(s_id = '%s')\n" % s_id )
     i = int(s.pop().value)
+    assert i <  999999999999, "int overflow > 999999999999"
+    assert i > -999999999999, "int underflow < -999999999999"
     s.push(StackObject(i,TInt))
 op_int.sig=TypeSignature([TAtom],[TInt])
 
@@ -80,8 +82,23 @@ def op_plus(s: Stack, s_id: str) -> None:
     print("op_plus(s_id = '%s')\n" % s_id) 
     op1 = s.pop().value
     op2 = s.pop().value
-    s.push(StackObject(op1+op2,TInt))
-op_plus.sig=TypeSignature([TInt,TInt],[TInt])    
+    result = op1+op2
+    # Guarantee output is valid and not overflow.
+    assert int(result) - op2 == op1, "python math error"
+    s.push(StackObject(result,TInt))
+    op_int(s,s_id)
+op_plus.sig=TypeSignature([TInt,TInt],[TInt])   
+
+def op_minus(s: Stack, s_id: str) -> None:
+    print("op_minus(s_id = '%s')\n" % s_id) 
+    op1 = s.pop().value
+    op2 = s.pop().value
+    result = op2-op1
+    # Guarantee output is valid and not overflow.
+    assert int(result) + op1 == op2, "python math error"
+    s.push(StackObject(result,TInt))
+    op_int(s,s_id)
+op_minus.sig=TypeSignature([TInt,TInt],[TInt])   
 
 def op_print(s: Stack, s_id: str) -> None:
     print("op_print(s_id = '%s')\n" % s_id) 
@@ -132,3 +149,4 @@ forth_dict.insert(0,('drop',op_drop))
 
 #   Int dictionary
 TInt.forth_dict.insert(0,('+',op_plus))
+TInt.forth_dict.insert(0,('-',op_minus))
