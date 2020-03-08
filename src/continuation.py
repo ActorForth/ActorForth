b166aab598ce36b93abec11562bde949d8209f8c
Ben Scherrey <scherrey@proteus-tech.com>
2020-03-08 15:24:15 +0700

    Forgot to add continuation.py to git.

---
+++ src/continuation.py
@@ -0,0 +1,58 @@
+#
+#	continuation.py		-	Continuation state of our environment.
+#
+
+from typing import Dict, List, Tuple, Callable, Any, Optional
+
+from dataclasses import dataclass
+
+from stack import Stack
+
+Op_name = str
+Operation_def = Callable[["Continuation"],None]
+
+class Operation:
+
+    def __init__(self, name: Op_name, op: Operation_def, words: List["Operation"] = None) -> None:
+        self.name = name
+        self.the_op : Operation_def = op
+        self.words : List["Operation"] = words or []
+
+    def add_word(self, op: "Operation") -> bool:
+        # Should check for valid stack type signature.
+        self.words.append(op)
+        return True
+
+    #def __call__(self, stack: Stack, name: Op_name) -> None:
+    def __call__(self, cont: "Continuation") -> None:
+        self.the_op(cont)
+
+    def __str__(self) -> str:
+        result = "Op{'%s':(%s)" % (self.name, self.the_op.__qualname__)
+        #if len(self.words)>0:
+        result += str(self.words)
+        result += "}"
+        return result
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def short_name(self) -> str:
+    	return self.name
+
+def nop(c: "Continuation") -> None:
+    pass
+
+
+@dataclass
+class Continuation:
+	stack : Stack
+	op : Operation = Operation("nop",nop)
+
+	def __str__(self) -> str:
+		result = "Cont: %s" % self.op
+		result += "\n      Stack(%s) = %s " \
+			% (len(self.stack.contents()),self.stack.contents())
+		return result			
+
+	
