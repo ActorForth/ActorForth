05b290b465a4af63ef3ce8e3240dfd713e33e18a
Ben Scherrey <scherrey@proteus-tech.com>
2020-04-11 21:39:16 +0700

    Initial object dependency working.

---
+++ src/aftype.py
@@ -0,0 +1,44 @@
+from typing import Callable, List, Optional, Any
+from dataclasses import dataclass
+
+from stack import Stack
+
+@dataclass(frozen = True)
+class Location:
+    filename : str = "Unknown"
+    linenum : int = 0
+    column : int = 0
+
+
+@dataclass(order = True)
+class Symbol:
+    s_id : str
+    location : Location 
+    #type : Type 
+    
+    @property
+    def size(self) -> int:
+        return len(self.s_id)
+
+    def __eq__(self, symbol = None) -> bool:
+        #if type(symbol) is Symbol:
+        #    return symbol.s_id == self.s_id
+        return symbol == self.s_id  
+
+
+class AF_Type:
+    def __init__(self):
+        pass
+
+@dataclass 
+class AF_Continuation:
+    stack : Stack
+    symbol : Optional[Symbol] = None
+    op : Any = None # Becomes an Operation in Continuation
+
+    debug : bool = False
+    ddepth : int = 0        # Depth of calls for debug tab output.
+
+    
+    def __init__(self):
+        pass
---
+++ src/operation.py
@@ -0,0 +1,71 @@
+from typing import Dict, List, Tuple, Callable, Any, Optional, Sequence
+from dataclasses import dataclass
+
+from aftype import AF_Type, AF_Continuation
+
+from stack import Stack
+
+
+@dataclass
+class TypeSignature:
+    stack_in : Sequence["AF_Type"]
+    stack_out : Sequence["AF_Type"]
+
+    def match_in(self, stack: Stack) -> bool:
+        if not len(self.stack_in): return True
+        stack_types = [s.type for s in stack.contents()[len(self.stack_in)*-1:] ]
+
+        print("\nmatch_in: in_types = %s" % (self.stack_in))
+        print("match_in: stack_types = %s" % stack_types)
+        for in_type in reversed(self.stack_in):
+            ## This is now handled in Type class overloads!!
+            ## if in_type == TAny: continue
+            """
+            Should probably have TAny types transform to the discovered type
+            so that manipulations across generics are still completely type safe.
+            """
+            stack_type = stack_types.pop()
+            if in_type != stack_type:
+                print("match_in: Stack type %s doesn't match input arg type %s." % (type,in_type))
+                return False
+        print("match_in: Found matching type for stack_in: %s" % self.stack_in)
+        return True
+
+    def match_out(self, on_stack_types: List["AF_Type"]) -> bool:
+        return True
+
+
+Op_name = str
+Operation_def = Callable[["AF_Continuation"],None]
+
+class Operation:
+
+    def __init__(self, name: Op_name, op: Operation_def, words: List["Operation"] = None) -> None:
+        self.name = name
+        self.the_op : Operation_def = op
+        self.words : List["Operation"] = words or []
+
+    def add_word(self, op: "Operation") -> bool:
+        # Should check for valid stack type signature.
+        self.words.append(op)
+        return True
+
+    def __call__(self, cont: "AF_Continuation") -> None:
+        self.the_op(cont)
+
+    def __str__(self) -> str:
+        result = "Op{'%s':(%s)" % (self.name, self.the_op.__qualname__)
+        result += str(self.words)
+        result += "}"
+        return result
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def short_name(self) -> str:
+        return self.name        
+
+
+Op_list = List[Tuple[Operation, TypeSignature]]
+
+Op_map = List[Tuple[Sequence["AF_Type"],Operation]]
