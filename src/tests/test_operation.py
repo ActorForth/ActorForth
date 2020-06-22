d0551f728b1afc79efc6a773b56ac0e2a5c9bc12
Ben Scherrey <scherrey@proteus-tech.com>
2020-06-22 12:32:44 +0700

    Added tests/test_operation.py.

---
+++ src/tests/test_operation.py
@@ -0,0 +1,17 @@
+import unittest
+
+from operation import TypeSignature, Operation, op_nop
+
+from af_types.af_bool import *
+from af_types.af_int import *
+from af_types.af_any import *
+
+class OperationTests(unittest.TestCase):
+
+
+    def test_simple_stack_effect(self) -> None:
+        op = Operation("nop", op_nop, sig=TypeSignature([TInt],[TInt]))
+
+        sig, match = op.check_stack_effect()
+        assert sig == [TInt]
+        assert match == False
