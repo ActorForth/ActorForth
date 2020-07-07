9811fcb87a2ecccdd42b935f5b05e96eb6f14ec9
Ben Scherrey <scherrey@proteus-tech.com>
2020-07-07 16:15:21 +0700

    Small refactor for af_bool.py. Issue 4 likely not complete.

--- src/af_types/af_bool.py
+++ src/af_types/af_bool.py
@@ -28,6 +28,12 @@ def op_bool(c: AF_Continuation) -> None:
     assert result.value is not None, "%s is not a valid Boolean value." % stack_object.value
 
     c.stack.push(result)
+Type.register_ctor('Bool',Operation('bool',op_bool),[TAny])
+
+### TODO :  if issue 4 ( https://github.com/ActorForth/ActorForth/issues/4 ) 
+###         is working properly then we shouldn't need this, right?
+Type.add_op(Operation('bool', op_bool, sig=TypeSignature([TAny],[TBool]) ))
+
 
 # TODO : issue #17 wait for created gerneralize type infer
 def optionally_infer_type_from_atom(c: AF_Continuation) -> StackObject:
@@ -50,7 +56,6 @@ def optionally_infer_type_from_atom(c: AF_Continuation) -> StackObject:
         sobj1 = c.stack.pop()
         c.log.debug("Converted from %s to %s." % (sobj1,c.stack.tos().type))
 
-
     return sobj1
 
 
@@ -60,46 +65,46 @@ def op_equals(c: AF_Continuation) -> None:
     # possibly been inferred.
     sobj2 = c.stack.pop()
     c.stack.push(StackObject(sobj1 == sobj2, TBool))
+Type.add_op(Operation('==', op_equals, sig=TypeSignature([TAny,TAny],[TBool]) ))    
+
 
 def op_not_equals(c: AF_Continuation) -> None:
     op_equals(c)
     result = c.stack.tos()
     result.value = not result.value
+Type.add_op(Operation('!=', op_not_equals, sig=TypeSignature([TAny,TAny],[TBool]) ))
+
 
 def op_less_than(c: AF_Continuation) -> None:
     sobj1 = optionally_infer_type_from_atom(c)
     sobj2 = c.stack.pop()
     c.log.debug("is %s (%s) < %s (%s)?" % (sobj2.value, type(sobj2.value), sobj1.value, type(sobj1.value)))
     c.stack.push(StackObject(sobj2.value < sobj1.value, TBool))
+Type.add_op(Operation('<', op_less_than, sig=TypeSignature([TAny,TAny],[TBool]) ))    
+
 
 def op_greater_than(c: AF_Continuation) -> None:
     sobj1 = optionally_infer_type_from_atom(c)
     sobj2 = c.stack.pop()
     c.stack.push(StackObject(sobj2.value > sobj1.value, TBool))
+Type.add_op(Operation('>', op_greater_than, sig=TypeSignature([TAny,TAny],[TBool]) ))    
+
 
 def op_less_than_or_equal_to(c: AF_Continuation) -> None:
     sobj1 = optionally_infer_type_from_atom(c)
     sobj2 = c.stack.pop()
     c.stack.push(StackObject(sobj2.value <= sobj1.value, TBool))
+Type.add_op(Operation('<=', op_less_than_or_equal_to, sig=TypeSignature([TAny,TAny],[TBool]) ))    
+
 
 def op_greater_than_or_equal_to(c: AF_Continuation) -> None:
     sobj1 = optionally_infer_type_from_atom(c)
     sobj2 = c.stack.pop()
     c.stack.push(StackObject(sobj2.value >= sobj1.value, TBool))
+Type.add_op(Operation('>=', op_greater_than_or_equal_to, sig=TypeSignature([TAny,TAny],[TBool]) ))
 
 
 def op_not(c: AF_Continuation) -> None:
     # Restrict to only workong on Bools!
     op1 = c.stack.tos().value = not c.stack.tos().value
 Type.add_op(Operation('not', op_not, sig=TypeSignature([TBool],[TBool]) ), "Bool")
-
-
-#   Bool dictionary
-Type.register_ctor('Bool',Operation('bool',op_bool),[TAny])
-Type.add_op(Operation('bool', op_bool, sig=TypeSignature([TAny],[TBool]) ))
-Type.add_op(Operation('==', op_equals, sig=TypeSignature([TAny,TAny],[TBool]) ))
-Type.add_op(Operation('!=', op_not_equals, sig=TypeSignature([TAny,TAny],[TBool]) ))
-Type.add_op(Operation('<', op_less_than, sig=TypeSignature([TAny,TAny],[TBool]) ))
-Type.add_op(Operation('>', op_greater_than, sig=TypeSignature([TAny,TAny],[TBool]) ))
-Type.add_op(Operation('<=', op_less_than_or_equal_to, sig=TypeSignature([TAny,TAny],[TBool]) ))
-Type.add_op(Operation('>=', op_greater_than_or_equal_to, sig=TypeSignature([TAny,TAny],[TBool]) ))
