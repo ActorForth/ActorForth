9d4bfc9cc637012e5f9c998937bfa6bf495a106a
Benjamin Scherrey <scherrey@proteus-tech.com>
2020-08-27 00:21:44 +0700

    Used walrus operator to improve Parser. Fixed up source_python_path to export environment variables properly.

--- src/parser.py
+++ src/parser.py
@@ -28,12 +28,14 @@ class Parser:
         self.reset()
         self.filename = filename
         self.file_handle = open(filename)
+        assert self.file_handle
         return self
 
     def open_handle(self, handle : TextIO, filename: str = None) -> "Parser":
         self.reset()
         self.filename = filename or "Unknown"
         self.file_handle = handle
+        assert self.file_handle
         return self
 
     def tokens(self) -> Iterator[Tuple[str, int, int]]:
@@ -41,57 +43,50 @@ class Parser:
         Generator yielding tuples of 
           (token : str, linenum : int, token_column : int)
         """
-
-        def get_char():
-            while True:
-                last_char = self.file_handle.read(1)
-                if not last_char:
-                    break
-                yield last_char
+        assert self.file_handle
 
         token = ""
         token_column = 0
         linenum = 1
         column = 1
         white_space = True
-        while True:
-            for char in get_char():
+        
+        while char := self.file_handle.read(1):
 
-                if char == ' ' or char == '\t' or char == '\n':
-                    column += 1 # \n will reset column later.
-                    # Tabs are assumed to occur on every 4th character
-                    # for purposes of column counting.
-                    if char == '\t': column += 4 - (column % 4)
-                    white_space = True
+            if char == ' ' or char == '\t' or char == '\n':
+                column += 1 # \n will reset column later.
+                # Tabs are assumed to occur on every 4th character
+                # for purposes of column counting.
+                if char == '\t': column += 4 - (column % 4)
+                white_space = True
 
-                # Punctuation doesn't need whitespace!
-                elif char == '.' or char == ':' or char == ';':
-                    white_space = False
+            # Punctuation doesn't need whitespace!
+            elif char == '.' or char == ':' or char == ';':
+                white_space = False
 
-                    if token:
-                        # Flush out the previous token.
-                        yield (token, linenum, token_column)
-                    # Start a new token
-                    token = "".join(char)
-                    token_column = column
-                    column += 1
-
-                else:
-                    if white_space:
-                        token_column = column
-                    column += 1
-                    token += char
-                    white_space = False
-                    
-                if token and white_space:
+                if token:
+                    # Flush out the previous token.
                     yield (token, linenum, token_column)
-                    token = ""
+                # Start a new token
+                token = "".join(char)
+                token_column = column
+                column += 1
 
-                if char == '\n':
-                    linenum += 1
-                    column = 1  # reset the column as promised.
+            else:
+                if white_space:
+                    token_column = column
+                column += 1
+                token += char
+                white_space = False
+                
+            if token and white_space:
+                yield (token, linenum, token_column)
+                token = ""
 
-            # Handle any incremental token that may be left over.
-            if token:
-                yield (token, linenum, token_column)              
-            break
+            if char == '\n':
+                linenum += 1
+                column = 1  # reset the column as promised.
+
+        # Handle any incremental token that may be left over.
+        if token:
+            yield (token, linenum, token_column)                      
