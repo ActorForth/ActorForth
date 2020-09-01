b35f57b7c8476612faa9d27ddf229abe05f39d7b
Ben Scherrey <scherrey@proteus-tech.com>
2020-09-01 22:01:54 +0700

    Change so if restore is called on the last checkpoint it automatically creates a new one.

--- src/af_types/af_environment.py
+++ src/af_types/af_environment.py
@@ -43,10 +43,14 @@ def op_restore(c: AF_Continuation) -> None:
 	checkpoint = checkpoints.pop()
 	Type.types = checkpoint[0]
 	Type.ctors = checkpoint[1]
+
 	## TODO : This doesn't seem to be resetting our stacks.
 	s = Stack()
 	r = Stack()
 	c = Continuation(stack=s, rstack=r)
+
+	if checkpoints.depth() == 0:
+		op_checkpoint(c)
 make_word_context('restore', op_restore, [], [])
 
 
