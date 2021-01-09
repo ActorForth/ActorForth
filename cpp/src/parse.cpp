d965672aa4cf5062f01c5020f961d37537389039
Ben Scherrey <scherrey@proteus-tech.com>
2021-01-09 17:36:56 +0700

    Got some strange error with generator.

--- cpp/src/parse.cpp
+++ cpp/src/parse.cpp
@@ -17,16 +17,53 @@ using felspar::makham::generator;
 class Parser
 {
 public:
-	Parser(const std::string filename ) : input( std::ifstream(filename, std::ios::binary) )
+	Parser(const std::string filename ) 
+		: input( std::ifstream(filename, std::ios::binary) ),
+		  location(filename)
 	{;}
 
 	char get() {return input.get();}
 	operator bool() const {return static_cast<bool>(input);}
 
+	struct FilePosition
+	{
+		FilePosition() : filename("=Unknown="), linenumber(1), column(1) {;}
+		FilePosition(const std::string& name) : filename(name), linenumber(1), column(1) {;}
+		const std::string filename;
+		unsigned linenumber;
+		unsigned column;
+	};
+
+	struct Token
+	{
+		const std::string value;
+		const FilePosition location;
+	};
+
+	generator<Token> tokens()
+	{
+		while(true)
+		{
+			if (not input) break;
+			char n = input.get();
+			if (n=='\n')
+			{
+				location.column = 1;
+				Token result = {"\n", location};
+				co_yield result;
+				location.linenumber += 1;
+			}
+
+		}
+	}
 
 private:
 	//std::variant<std::ifstream, std::istringstream> input;
 	std::ifstream input;
+
+
+
+	FilePosition location;
 };
 
 int main()
