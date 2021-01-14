3ce1d19a15ce6d11c3c697b9e5ddc164efdc4556
Benjamin Scherrey <scherrey@proteus-tech.com>
2021-01-14 17:26:42 +0700

    Initial stack implementation.

--- cpp/include/stack.hpp
+++ cpp/include/stack.hpp
@@ -15,26 +15,50 @@ template <class T> class Stack
 public:
 	struct Underflow : public std::out_of_range {;};
 
-	struct MaybeEmpty;
-	using StackResult = std::pair<MaybeEmpty, T&>;
+	struct Empty;
 	struct NonEmpty;
+	using MaybeEmpty = std::variant<Empty, NonEmpty>;
+
+	//using StackResult = std::pair<MaybeEmpty, T&>;
+
 	struct Empty
 	{
 		T& tos(void) const { throw Underflow(); }	
-		T& pop(void) const { throw Underflow(); }
-		NonEmpty push( const T& value) { return NonEmpty(value); }
+		MaybeEmpty pop(void) { throw Underflow(); }
+		MaybeEmpty push( const T& value )
+		{ 
+			auto r = NonEmpty(value); 
+			return { r, r.tos() };
+		}
+		MaybeEmpty push( const T&& value )
+		{ 
+			auto r = NonEmpty(value); 
+			return { r, r.tos() };
+		}
 	};
 
 	struct NonEmpty
 	{
-		NonEmpty( const T& value ) {;}
+		NonEmpty( const T& value ) : _data(1,value) {;}
+		NonEmpty( const T&& value ) { _data.emplace_back(value); }
+		T& tos(void) const { return _data.back(); }	
+		MaybeEmpty pop(void) const 
+		{ 
+			_data.pop_back(); 
+			if(_data.empty()) return Empty();
+			return *this;
+		}
+		MaybeEmpty push( const T& value ) { _data.emplace_back(value); return *this; }
+		MaybeEmpty push( const T&& value ) { _data.emplace_back(value); return *this; }
+		std::vector<T> _data;
 	};
 
-	using MaybeEmpty = std::variant<Empty, NonEmpty>;
+	
 
 	Stack(void) : _stack(Empty()) {;} // { _stack = Empty();}
+	Stack(const Stack&) = default;
 
 
-private:
+//private:
 	MaybeEmpty _stack;
 };
--- cpp/tests/test_stack.cpp
+++ cpp/tests/test_stack.cpp
@@ -8,7 +8,16 @@
 
 int main(void)
 {
-	Stack stack;
+	std::vector<int> v(1,12);
+	std::vector<int> w;
+
+	std::cout << "My vector<int> : ";
+	for( auto n: v) std::cout << n << std::endl;
+	std::cout << "It's capacity is " << v.capacity() << std::endl;
+	std::cout << "The other vector's capacity is " << w.capacity() << std::endl;
+
+	Stack<char> stack;
 	std::cout << "Made a stack!" << std::endl;
+	//std::cout << "It's capacity is " << stack._stack.capacity() << std::endl;
 	return 0;
 }
