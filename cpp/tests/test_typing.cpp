1519fc5faedd3533ef7fefc1d628a27725fe86c6
Benjamin Scherrey <scherrey@proteus-tech.com>
2021-01-26 16:00:34 +0700

    Initial test_typing to figure out how to extract our variants.

---
+++ cpp/tests/test_typing.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+#include <variant>
+
+using namespace std;
+
+using Object = std::variant< bool, int, string >;
+
+template <class T> struct Type
+{
+	Type(const string& t) : name(t) {;}
+	T& value( Object& o) { return std::get<T>(o); }
+
+	const std::string name;
+};
+
+int main()
+{
+	Object ob = true;
+	Type<bool> tb("Boolean");
+
+	Object oi = 42;
+	Type<int> ti("Integer");
+
+	Object os = "A string.";
+	Type<string> ts("String");
+
+	cout << "Here's a " << tb.name << " : " << tb.value(ob) << endl;
+	cout << "Here's a " << ti.name << " : " << ti.value(oi) << endl;
+	cout << "Here's a " << ts.name << " : " << ts.value(os) << endl;
+	
+	return 0;
+}
