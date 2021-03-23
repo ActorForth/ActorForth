ca7060edeb736ba56a824f13b8e5bfb40d6fece4
Ben Scherrey <scherrey@proteus-tech.com>
2021-03-23 10:30:34 +0700

    Added Cryptoledger mind map doc in Freemind format.

---
+++ docs/design/AFCryptoledger.mm
@@ -0,0 +1,56 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1616298420673" ID="ID_1120283703" MODIFIED="1616298980288" TEXT="Cryptoledger">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1616298447761" ID="ID_838255742" MODIFIED="1616298974913" POSITION="right" TEXT="Extensibility">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1616298461602" ID="ID_1794024256" MODIFIED="1616299000346" TEXT="Hypoiconic">
+<edge STYLE="bezier" WIDTH="thin"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1794024256" ENDARROW="Default" ENDINCLINATION="87;-234;" ID="Arrow_ID_593856693" SOURCE="ID_1778722697" STARTARROW="None" STARTINCLINATION="87;-316;"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1616299047512" ID="ID_1513651632" MODIFIED="1616299047513" TEXT="">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1616298854590" ID="ID_1778722697" MODIFIED="1616299000345" POSITION="left" TEXT="ActorForth">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<arrowlink DESTINATION="ID_1794024256" ENDARROW="Default" ENDINCLINATION="87;-234;" ID="Arrow_ID_593856693" STARTARROW="None" STARTINCLINATION="87;-316;"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1616299704738" ID="ID_1402577242" MODIFIED="1616299722950" POSITION="left" TEXT="Challenges of Existing Languages">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1616299490834" ID="ID_687719266" MODIFIED="1616299500033" POSITION="right" TEXT="Challanges of Existing Cryptoledgers">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1616299506488" ID="ID_1080845060" MODIFIED="1616299522397" TEXT="upgrading software is hard">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1616299533268" ID="ID_1096801297" MODIFIED="1616299549124" TEXT="user/node abandonment">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1616299576432" ID="ID_1741804046" MODIFIED="1616299606172" TEXT="multiple target environments">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1616299607488" ID="ID_130549268" MODIFIED="1616299607489" TEXT="">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1616300130427" ID="ID_1282230179" MODIFIED="1616300143501" TEXT="Accidental Centralization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1616300147965" ID="ID_349647532" MODIFIED="1616300165329" TEXT="Oracle-style Validators">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1616300166112" ID="ID_27004902" MODIFIED="1616300194798" TEXT="Monolithic core codebase">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
