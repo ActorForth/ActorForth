3df52bf1529462d2d7b3757222fe65bc3d3cdd9b
Ben Scherrey <scherrey@biggestlab.io>
2025-01-17 20:15:05 +0700

    Playing with interpreting tokens.

--- src/actorforth.erl
+++ src/actorforth.erl
@@ -1,20 +1,47 @@
 -module(actorforth).
 
--export([interpret/1]).
+-export([interpret/1, make_token/1, make_token/2, make_token/3, make_token/4]).
 
--spec interpret([string()]) -> ok.
+-record(token, {
+    value :: string(),
+    column :: non_neg_integer(),
+    line :: non_neg_integer(),
+    file :: string()
+}).
+
+-spec interpret([#token{}]) -> ok.
 interpret(Tokens) ->
-    lists:foreach( fun interpret_token/1, Tokens),
+    lists:foreach(fun interpret_token/1, Tokens),
     io:format("ok.~n"),
     ok.
 
-
--spec interpret_token(string()) -> ok.
-interpret_token(Token) ->
+-spec interpret_token(#token{}) -> ok.
+interpret_token(#token{value = Token, line = Line, column = Column, file = File}) ->
     case string:to_integer(Token) of
         {Int, []} -> % Conversion successful, no remaining characters
-            io:format("int(~p) ", [Int]);
+            io:format("int(~p) at ~s, line ~p, column ~p ", [Int, File, Line, Column]);
         _ -> % Conversion failed or partial
-            io:format("~s ", [Token])
+            io:format("'~s' at ~s, line ~p, column ~p ", [Token, File, Line, Column])
     end,
     ok.
+
+-spec make_token(string()) -> #token{}.
+make_token(Value) ->
+    make_token(Value, 0, 0, "(unknown)").
+
+-spec make_token(string(), non_neg_integer()) -> #token{}.
+make_token(Value, Line) ->
+    make_token(Value, Line, 0, "(unknown)").
+
+-spec make_token(string(), non_neg_integer(), non_neg_integer()) -> #token{}.
+make_token(Value, Line, Column) ->
+    make_token(Value, Line, Column, "(unknown)").
+
+-spec make_token(string(), non_neg_integer(), non_neg_integer(), string()) -> #token{}.
+make_token(Value, Line, Column, File) ->
+    #token{
+        value = Value,
+        column = Column,
+        line = Line,
+        file = File
+    }.
