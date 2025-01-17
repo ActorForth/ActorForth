d30fa2d8488739e3f6389c6488d3e4f57ee6f64e
Ben Scherrey <scherrey@biggestlab.io>
2025-01-17 19:58:23 +0700

    Playing with actorforth:interpret.

--- src/actorforth.erl
+++ src/actorforth.erl
@@ -1,3 +1,20 @@
 -module(actorforth).
 
--export([]).
+-export([interpret/1]).
+
+-spec interpret([string()]) -> ok.
+interpret(Tokens) ->
+    lists:foreach( fun interpret_token/1, Tokens),
+    io:format("ok.~n"),
+    ok.
+
+
+-spec interpret_token(string()) -> ok.
+interpret_token(Token) ->
+    case string:to_integer(Token) of
+        {Int, []} -> % Conversion successful, no remaining characters
+            io:format("int(~p) ", [Int]);
+        _ -> % Conversion failed or partial
+            io:format("~s ", [Token])
+    end,
+    ok.
