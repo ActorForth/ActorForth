42abc0f7107a2ee7d8ec3cf71e44a0f8f9b60d36
Benjamin Scherrey <scherrey@biggestlab.io>
2025-01-18 11:51:02 +0700

    Created thread:make_continuation.

--- src/thread.erl
+++ src/thread.erl
@@ -1,42 +1,50 @@
 -module(thread).
 
--include("token.hrl"). 
+-include("token.hrl").
 
--export([empty_continuation/0]).
+-export([make_continuation/0, make_continuation/1, make_continuation/2, make_continuation/3, make_continuation/4]).
 
 -export_type([type/0, thread/0, continuation/0]).
 
-
 -type type() :: atom().
--type stack_item() :: {type(), term() | function()}.
-
 -type thread() :: [function()].
-
 
 -record(continuation, {
     data_stack :: list(),
     return_stack :: list(),
     next_op :: reference() | 'end',
-    current_token :: repl:token()
+    current_token :: #token{}
 }).
 
 -type op_ref() :: reference() | 'end'.
 -type continuation() :: #continuation{
-    data_stack :: stack:stack(),
-    return_stack :: stack:stack(),
+    data_stack :: list(),
+    return_stack :: list(),
     next_op :: op_ref(),
-    current_token :: repl:token()
+    current_token :: #token{}
 }.
 
-empty_continuation() ->
+-spec make_continuation() -> #continuation{}.
+make_continuation() ->
+    make_continuation([], [], 'end', #token{value = "", column = 0, line = 0, file = "(unknown)"}).
+
+-spec make_continuation(#token{}) -> #continuation{}.
+make_continuation(#token{} = CurrentToken) ->
+    make_continuation([], [], 'end', CurrentToken).
+
+-spec make_continuation(DataStack :: list(), ReturnStack :: list()) -> #continuation{}.
+make_continuation(DataStack, ReturnStack) ->
+    make_continuation(DataStack, ReturnStack, 'end', #token{value = "", column = 0, line = 0, file = "(unknown)"}).
+
+-spec make_continuation(DataStack :: list(), ReturnStack :: list(), NextOp :: op_ref()) -> #continuation{}.
+make_continuation(DataStack, ReturnStack, NextOp) ->
+    make_continuation(DataStack, ReturnStack, NextOp, #token{value = "", column = 0, line = 0, file = "(unknown)"}).
+
+-spec make_continuation(DataStack :: list(), ReturnStack :: list(), NextOp :: op_ref(), CurrentToken :: #token{}) -> #continuation{}.
+make_continuation(DataStack, ReturnStack, NextOp, CurrentToken) ->
     #continuation{
-        data_stack = [],
-        return_stack = [],
-        next_op = 'end',
-        current_token = #token{
-            value = "",
-            column = 0,
-            line = 0,
-            file = "(unknown)"
-        }
+        data_stack = DataStack,
+        return_stack = ReturnStack,
+        next_op = NextOp,
+        current_token = CurrentToken
     }.
